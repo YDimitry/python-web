bcbb7ae8b123818e292f3eac564bdccea077c9fb
Dm <YDimitry@mail.ru>
2020-02-25 16:02:31 +0300

    мелкие исправления

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo /etc/init.d/nginx restart
+sudo /etc/init.d/nginx restart
 #sudo systemctl restart nginx
 
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
