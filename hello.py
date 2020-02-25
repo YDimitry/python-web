ca764dd3d6f77148f6e0c10ccfae039c92b62224
Dm <YDimitry@mail.ru>
2020-02-25 22:09:46 +0300

    изменен Context-Type с text/html на text/plain

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 def application(env, start_response):
 
     data = '\n'.join(env['QUERY_STRING'].split('&')).encode('utf-8')
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8'),
+    start_response('200 OK', [('Content-Type', 'text/plain'),
     ("Content-Length", str(len(data)))])
 
 
