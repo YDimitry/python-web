a065e4f403345f4b82cddfce23497ff365040a7c
Dm <ydimitry@mail.ru>
2020-03-01 23:37:44 +0300

    добавлен QuestionManager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,18 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
-    title = models.CharField(max_length=255)  # заголовок вопроса
-    text = models.TextField()  # полный текст вопроса
-    added_at = models.DateField(blank=True, auto_now_add=True)  # дата добавления вопроса
-    rating = models.IntegerField(default=0)  # рейтинг вопроса(число)
-    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)  # автор вопроса
-    likes = models.ManyToManyField(User, related_name='question_like_user')  # список пользователей, поставивших "лайк"
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
 
 
 class Answer(models.Model):
-    text = models.TextField()  # текст ответа
-    added_at = models.DateField(blank=True,auto_now_add=True)  # дата добавления ответа
-    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)  # вопрос, к которому относится ответ
-    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)  # автор ответа
-
+    text = models.TextField()
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
--- init.sh
+++ init.sh
@@ -5,20 +5,23 @@ sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -s ~/web/etc/helloapp /etc/init.d/helloapp
 #sudo ln -s ~/web/etc/gunicorn.service /etc/systemd/system/gunicorn.service
 sudo /etc/init.d/mysql start
-mysql -uroot -e "create database stepic_web;"
-mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
-~/web/ask/manage.py makemigrations
-~/web/ask/manage.py migrate
 
 sudo /etc/init.d/helloapp start
 sudo /etc/init.d/nginx restart
 sudo apt update
-sudo apt install python3.5
-sudo apt install mc
+sudo apt install -y python3.5
+sudo apt install -y python3.5-dev
+sudo apt install -y mc
 
 virtualenv -p python3.5 env
 source env/bin/activate
-pip install gunicorn django==2.1
+pip install gunicorn django==2.0 mysqlclient
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
+
 cd ~/web/ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi
 
