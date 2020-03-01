9fa3786d9496625edf8001793e0024d96fad677f
Dm <ydimitry@mail.ru>
2020-03-01 23:37:44 +0300

    добавлен QuestionManager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,7 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)  # заголовок вопроса
     text = models.TextField()  # полный текст вопроса
     added_at = models.DateField(blank=True, auto_now_add=True)  # дата добавления вопроса
@@ -13,7 +22,6 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()  # текст ответа
-    added_at = models.DateField(blank=True,auto_now_add=True)  # дата добавления ответа
+    added_at = models.DateField(blank=True, auto_now_add=True)  # дата добавления ответа
     question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)  # вопрос, к которому относится ответ
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)  # автор ответа
-
