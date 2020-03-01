8b90306865fd2fee3ee502ff2133a06138da2139
Dm <ydimitry@mail.ru>
2020-03-01 21:25:36 +0300

    добавлены django модели для приложения qa

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)  # заголовок вопроса
+    text = models.TextField()  # полный текст вопроса
+    added_at = models.DateField(blank=True, auto_now_add=True)  # дата добавления вопроса
+    rating = models.IntegerField(default=0)  # рейтинг вопроса(число)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)  # автор вопроса
+    likes = models.ManyToManyField(User, related_name='question_like_user')  # список пользователей, поставивших "лайк"
+
+
+class Answer(models.Model):
+    text = models.TextField()  # текст ответа
+    added_at = models.DateField(blank=True,auto_now_add=True)  # дата добавления ответа
+    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)  # вопрос, к которому относится ответ
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)  # автор ответа
+
--- init.sh
+++ init.sh
@@ -4,6 +4,12 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -s ~/web/etc/helloapp /etc/init.d/helloapp
 #sudo ln -s ~/web/etc/gunicorn.service /etc/systemd/system/gunicorn.service
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
+
 sudo /etc/init.d/helloapp start
 sudo /etc/init.d/nginx restart
 sudo apt update
